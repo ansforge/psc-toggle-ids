2015f375a6d534cbedbe566ea5f2bce0c4cc7c9c
bbonche <bbonche@henix.fr>
2022-01-12 21:50:39 +0100

    add directory in Dockerfile and chown

--- psc-toggle-manager/src/main/java/fr/ans/psc/toggle/service/ToggleService.java
+++ psc-toggle-manager/src/main/java/fr/ans/psc/toggle/service/ToggleService.java
@@ -30,7 +30,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ToggleService {
 
     private final String TOGGLE_FILE_NAME = "Table_de_Correspondance_bascule";
-    private final String FAILURE_REPORT_FILENAME = "/pscload_rapport_des_échecs_de_bascule.csv";
+    private final String FAILURE_REPORT_FILENAME = "pscload_rapport_des_échecs_de_bascule.csv";
     private static final int TOGGLE_ROW_LENGTH = 2;
 
     @Autowired
@@ -155,7 +155,7 @@ public class ToggleService {
             dataLines.add(String.join(";", dataItems));
         });
 
-        File csvOutputFile = new File(FAILURE_REPORT_FILENAME);
+        File csvOutputFile = new File("/app", FAILURE_REPORT_FILENAME);
         try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
             pw.println("original nationalId;target nationalId;code retour");
             dataLines.forEach(pw::println);
