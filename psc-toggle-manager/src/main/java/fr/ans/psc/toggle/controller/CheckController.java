21e2b754d55dc51aca58c2fd22ac31f6fcbd710f
Eric Degenetais <eric.degenetais.ext@esante.gouv.fr>
2024-03-20 13:27:04 +0100

    FIX it for now.

--- psc-toggle-manager/src/main/java/fr/ans/psc/toggle/controller/CheckController.java
+++ psc-toggle-manager/src/main/java/fr/ans/psc/toggle/controller/CheckController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © ${year} Agence du Numérique en Santé (ANS) (https://esante.gouv.fr)
+ * Copyright © 2022-2024 Agence du Numérique en Santé (ANS) (https://esante.gouv.fr)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
